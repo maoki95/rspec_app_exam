ca4ae33929cfc41d1c9e55ba90a6ddf354a39faf
古川真宙 <furukawamaoki@furukawamakotochuunoMacBook-Pro.local>
2021-02-25 16:52:24 +0900

    Task編集FIXME:done

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -55,13 +55,13 @@ RSpec.describe 'Task', type: :system do
   describe 'Task編集' do
     let!(:task_done) { create(:task, :done) }
     context '正常系' do
-      xit 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
+      it 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
         visit edit_project_task_path(project, task)
         fill_in 'Deadline', with: Time.current
         click_button 'Update Task'
         click_link 'Back'
-        expect(find('.task_list')).to have_content(Time.current.strftime('%Y-%m-%d'))
+        expect(find('.task_list')).to have_content(short_time(Time.current))
         expect(current_path).to eq project_tasks_path(project)
       end
 
