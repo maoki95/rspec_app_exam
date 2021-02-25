dc3eb0e5d781a49998cbb5ddf4d1c22f5a9a5b29
古川真宙 <furukawamaoki@furukawamakotochuunoMacBook-Pro.local>
2021-02-25 16:34:35 +0900

    TODO: FactoryBotのtraitを利用してください　done

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -53,6 +53,7 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task編集' do
+    let!(:task_done) { create(:task, :done) }
     context '正常系' do
       xit 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
@@ -76,8 +77,6 @@ RSpec.describe 'Task', type: :system do
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id, status: :done, completion_date: Time.current.yesterday)
         visit edit_project_task_path(project, task)
         select 'todo', from: 'Status'
         click_button 'Update Task'
