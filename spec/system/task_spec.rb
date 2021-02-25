2663eac26c3bf26473871379767ca6ddec83f6a9
古川真宙 <furukawamaoki@furukawamakotochuunoMacBook-Pro.local>
2021-02-25 17:06:52 +0900

    Task削除 FIXME:done

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -88,15 +88,14 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task削除' do
+    let!(:task) {create(:task) }
     context '正常系' do
       # FIXME: テストが失敗するので修正してください
-      xit 'Taskが削除されること' do
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
+      fit 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
-        expect(page).not_to have_content task.title
+        expect(find('.task_list')).not_to have_content task.title
         expect(Task.count).to eq 0
         expect(current_path).to eq project_tasks_path(project)
       end
