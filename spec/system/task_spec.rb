8c4bf3267b23b3c5f67999acdb3db1488767a152
古川真宙 <furukawamaoki@furukawamakotochuunoMacBook-Pro.local>
2021-02-25 17:12:30 +0900

    Task一覧FIXME:done

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -13,10 +13,11 @@ RSpec.describe 'Task', type: :system do
         expect(current_path).to eq project_tasks_path(project)
       end
 
-      xit 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
+      it 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
         # FIXME: テストが失敗するので修正してください
         visit project_path(project)
         click_link 'View Todos'
+        visit project_tasks_path(project)
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
         expect(current_path).to eq project_tasks_path(project)
@@ -91,7 +92,7 @@ RSpec.describe 'Task', type: :system do
     let!(:task) {create(:task) }
     context '正常系' do
       # FIXME: テストが失敗するので修正してください
-      fit 'Taskが削除されること' do
+      it 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
